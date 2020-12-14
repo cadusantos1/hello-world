26e3a5a0414673f9637306fb8c0357e65f170a6d
Carlos Eduardo Santos <cadupessoal@hotmail.com>
2020-12-14 20:33:56 -0300

    pages;

---
+++ projeto site institucional/pages/home.php
@@ -0,0 +1,88 @@
+<section class="banner-container">
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/bg-form.jpg');" class="banner-single"></div><!--banner-single-->
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/retro.jpg');" class="banner-single"></div><!--banner-single-->
+	<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/moon.jpg');" class="banner-single"></div><!--banner-single-->
+		<div class="overlay"></div><!--overlay-->	
+			<div class="center"></div>			
+			<form method="post">
+				<h2>Qual o seu melhor e-mail?</h2>
+				<input type="email" name="email" required />
+				<input type="hidden" name="identificador" value="form_home">
+				<input type="submit" name="acao" value="Cadastrar!">
+			</form>
+			<!--center-->
+			<div class="bullets"></div><!--bullets-->
+		</section><!--banner-principal-->
+
+		<section class="descricao-autor">
+			<div class="center">
+			<div class="w50 left">
+			<h2>Obi Wan Kenobi</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec lectus ut dolor sagittis sodales id euismod felis. Maecenas posuere lectus eget sapien sagittis scelerisque. Fusce rhoncus malesuada neque, a efficitur magna egestas ut. Cras pretium purus ac justo eleifend, eu dictum velit aliquam. Quisque dapibus, lorem nec elementum ultricies, magna mi consectetur tortor, eget luctus justo odio at elit. Duis in nulla semper, consectetur urna non, fringilla lacus. Pellentesque pretium nisi risus, a tristique ipsum sodales sed. Vivamus ac commodo orci.</p>
+				<p>Nullam quis varius nunc. Ut ut leo elit. Donec vel placerat dui, eu dictum dolor. Aliquam suscipit orci sit amet feugiat semper. Fusce euismod commodo consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In tempor ex ac elementum rutrum.</p>
+			</div><!--w50-->
+			<div class="w50 left">
+				<img class="right" src="<?php echo INCLUDE_PATH; ?>images/obi.jpg" />
+			</div><!--w50-->
+			<div class="clear"></div><!--clear-->
+			</div><!--center-->
+		</section><!--descricao-autor-->
+
+		<section class="especialidades">
+			<div class="center">
+			<h2 class="title">Minhas Especialidades</h2> 
+				<div class="w33 left box-especialidade">
+						<h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+						<h4>HTML5</h4>
+						<p>Vestibulum tincidunt vel ligula non tempus. Vivamus semper pulvinar metus sit amet scelerisque. Ut ut sollicitudin urna, vitae lobortis augue. Quisque mauris ligula, tristique nec rutrum quis, interdum et mi. Nam volutpat risus a dui pharetra, ac euismod purus consequat.</p>
+				</div><!--box-especialidade-->
+				<div class="w33 left box-especialidade">
+						<h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+						<h4>CSS3</h4>
+						<p>Vestibulum tincidunt vel ligula non tempus. Vivamus semper pulvinar metus sit amet scelerisque. Ut ut sollicitudin urna, vitae lobortis augue. Quisque mauris ligula, tristique nec rutrum quis, interdum et mi. Nam volutpat risus a dui pharetra, ac euismod purus consequat.</p>
+				</div><!--box-especialidade-->
+				<div class="w33 left box-especialidade">
+						<h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
+						<h4>JavaScript</h4>
+						<p>Vestibulum tincidunt vel ligula non tempus. Vivamus semper pulvinar metus sit amet scelerisque. Ut ut sollicitudin urna, vitae lobortis augue. Quisque mauris ligula, tristique nec rutrum quis, interdum et mi. Nam volutpat risus a dui pharetra, ac euismod purus consequat.</p>
+				</div><!--box-especialidade-->
+				<div class="w33 left box-especialidade">
+						<h3><img src="images/php7.png" style="width: 45px; height: 45px;"></i></h3>
+						<h4>PHP</h4>
+						<p>Vestibulum tincidunt vel ligula non tempus. Vivamus semper pulvinar metus sit amet scelerisque. Ut ut sollicitudin urna, vitae lobortis augue. Quisque mauris ligula, tristique nec rutrum quis, interdum et mi. Nam volutpat risus a dui pharetra, ac euismod purus consequat.</p>
+				</div><!--box-especialidade-->
+				<div class="clear"></div><!--clear-->
+			</div><!--center-->
+		</section><!--especialidades-->
+
+		<section class="extras">
+			<div class="center">
+				<div id="Depoimentos" class="w50 left depoimentos-container"> 
+					<h2 class="title">Depoimentos</h2>
+					<div class="depoimento-single"> 
+						<p class="depoimento-descricao">"Nullam quis varius nunc. Ut ut leo elit. Donec vel placerat dui, eu dictum dolor. Aliquam suscipit orci sit amet feugiat semper. Fusce euismod commodo consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In tempor ex ac elementum rutrum."</p>
+						<p class="nome-autor">Lorem Ipsum</p>
+					</div><!--depoimento-single-->
+					<div class="depoimento-single"> 
+						<p class="depoimento-descricao">"Nullam quis varius nunc. Ut ut leo elit. Donec vel placerat dui, eu dictum dolor. Aliquam suscipit orci sit amet feugiat semper. Fusce euismod commodo consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In tempor ex ac elementum rutrum."</p>
+						<p class="nome-autor">Lorem Ipsum</p>
+					</div><!--depoimento-single-->
+					<div class="depoimento-single"> 
+						<p class="depoimento-descricao">"Nullam quis varius nunc. Ut ut leo elit. Donec vel placerat dui, eu dictum dolor. Aliquam suscipit orci sit amet feugiat semper. Fusce euismod commodo consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In tempor ex ac elementum rutrum."</p>
+						<p class="nome-autor">Lorem Ipsum</p>
+					</div><!--depoimento-single-->
+				</div><!--w50-->
+
+				<div id="Servicos" class="w50 left servicos-container"> 
+					<h2 class="title">Serviços</h2>
+					<div class="servicos"> 
+					<ul>
+						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec lectus ut dolor sagittis sodales id euismod felis. Maecenas posuere lectus eget sapien sagittis scelerisque. Fusce rhoncus malesuada neque, a efficitur magna egestas ut.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec lectus ut dolor sagittis sodales id euismod felis. Maecenas posuere lectus eget sapien sagittis scelerisque. Fusce rhoncus malesuada neque, a efficitur magna egestas ut.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec lectus ut dolor sagittis sodales id euismod felis. Maecenas posuere lectus eget sapien sagittis scelerisque. Fusce rhoncus malesuada neque, a efficitur magna egestas ut.</li>
+					</ul>
+					</div><!--servicos-->
+				</div><!--w50-->
+				<div class="clear"></div><!--clear-->
+			</div><!--center-->
+		</section><!--extras-->
